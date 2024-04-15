42a21eab112424615423eb778fde2d6cbe0bb1cd
yon3030 <jason1025yon@gmail.com>
2024-04-15 09:04:47 +0200

    uninstall vuex and adding pinia and mitt

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
-// Vuetify
+//  ! pinia config
+import { createPinia } from "pinia";
+//  ! Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
+//  ! Mitt
+import mitt from "mitt";
+const Emitter = mitt();
 
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-createApp(App).use(vuetify).use(store).use(router).mount("#app");
+createApp(App)
+  .use(vuetify)
+  .provide("Emitter", Emitter)
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
