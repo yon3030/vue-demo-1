600e587c0b01780176ab78140c443bac00d7c413
yon3030 <jason1025yon@gmail.com>
2024-04-15 07:45:27 +0200

    uploading projects files

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
-createApp(App).use(store).use(router).mount("#app");
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).use(store).use(router).mount("#app");
