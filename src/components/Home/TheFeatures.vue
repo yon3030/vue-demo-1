c3f7f605ea5d6c8d6d9f3b77b47ed3be7474058f
yon3030 <jason1025yon@gmail.com>
2024-04-16 12:26:09 +0200

    creating features sections

---
+++ src/components/Home/TheFeatures.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="features">
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="features_shipping">
+            <ShippingIcons />
+            <span>Free Shipping & Returns</span>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="features_Guarante">
+            <GuaranteeIcon />
+            <span>Lowest Price Guarantee</span>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="features_Trophy">
+            <TrophyIcon />
+            <span>Longest Warranties Offer</span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import ShippingIcons from "@/svg/featuresIons/ShippingIcons.vue";
+import GuaranteeIcon from "@/svg/featuresIons/GuaranteeIcon.vue";
+import TrophyIcon from "@/svg/featuresIons/TrophyIcon.vue";
+export default {
+  name: "TheFeatures",
+  components: {
+    ShippingIcons,
+    GuaranteeIcon,
+    TrophyIcon,
+  },
+};
+</script>
+<style lang="scss">
+.features {
+  font-size: 25px;
+  span {
+    vertical-align: top;
+    margin-left: 10px;
+  }
+}
+.features_Guarante,
+.features_shipping {
+  position: relative;
+}
+.features_Guarante::after,
+.features_shipping::after {
+  content: "";
+  background: #dadada;
+  width: 2px;
+  height: 70%;
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <UpperBanner />
+  <TheFeatures />
 </template>
 
 <script>
 import UpperBanner from "@/components/Home/UpperBanner.vue";
+import TheFeatures from "@/components/Home/TheFeatures.vue";
 
 export default {
   name: "HomeView",
   components: {
     UpperBanner,
+    TheFeatures,
   },
 };
 </script>
