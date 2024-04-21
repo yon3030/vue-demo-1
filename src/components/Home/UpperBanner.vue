a9ef62e8781fdbbea780e684fd858f25b53ccad7
yon3030 <jason1025yon@gmail.com>
2024-04-21 19:02:04 +0200

    adding button to upper banner section and editing some css

--- src/components/Home/UpperBanner.vue
+++ src/components/Home/UpperBanner.vue
@@ -5,6 +5,13 @@
         <v-col cols="5">
           <div class="upper_banner_content">
             <h2><strong>Huge Saving </strong>on UHD Televisions</h2>
+            <p>Sale up to 70% off on selected items*</p>
+            <v-btn
+              density="combact"
+              class="upper_banner_main_btn"
+              variant="outlined"
+              >Shop Now</v-btn
+            >
           </div>
         </v-col>
       </v-row>
@@ -22,7 +29,7 @@ export default {};
   background-size: cover;
   background-position: left top;
   &_content {
-    padding: 120px 0;
+    padding: 100px 0;
 
     strong {
       color: #10ffda;
@@ -32,6 +39,19 @@ export default {};
       font-size: 50px;
       font-weight: 900;
     }
+    p {
+      color: white;
+      font-size: 20px;
+      margin-bottom: 40px;
+    }
+  }
+  &_main_btn {
+    // background: red !important;
+    height: 50px !important;
+    border-radius: 30px !important;
+    padding: 0 30px !important;
+    border-color: white !important;
+    color: white !important;
   }
 }
 </style>
